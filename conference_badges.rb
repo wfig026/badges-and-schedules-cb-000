eb4cbbeb55cdd37b96d76dd6783e891ed01d70b6
Will Figueiredo <will@wfigueiredo.com>
2018-09-16 00:35:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,24 @@
 # Write your code here.
-def batch_badge_creator(names)
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+def batch_badge_creator(speakers)
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  speakers.map do |name|
+    "Hello, my name is #{name}"
+  end
+end
 
+def assign_rooms
+  name.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to the room #{index + 1}!"
+end
+
+def printer(name)
+  batch_badge_creator(name).each do |badge|
+    puts badge
+  end
+  assign_rooms(name).each do |assignment|
+    puts assignment
+  end
 end
